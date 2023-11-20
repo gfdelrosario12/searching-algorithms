ca91a39fc1c9086755cd19d5a5d14dee60c9763d
Gladwin Ferdz Del Rosario <gladelrosario12@gmail.com>
2023-11-21 01:52:34 +0800

    update Binary Search code

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,5 +1,17 @@
 import java.util.Arrays;
 
 public class BinarySearch {
+    private int[] dataSet; // Use regular array syntax
+    private int numberToBeSearched;
 
+    public BinarySearch(int[] dataSet, int numberToBeSearched) {
+        this.dataSet = dataSet;
+        this.numberToBeSearched = numberToBeSearched;
+    }
+
+    private boolean search() {
+        Arrays.sort(this.dataSet);
+        int firstDivide = numberToBeSearched / this.dataSet[-1];
+        return false;
+    }
 }
