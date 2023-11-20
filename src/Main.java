97aea0cf8091cfbd7787e3915b39da2d84b0e9fe
Gladwin Ferdz Del Rosario <gladelrosario12@gmail.com>
2023-11-21 01:53:10 +0800

    updated main code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public int[] dataSet = {37, 62, 15, 89, 24, 50, 73, 5, 42, 78};
+    public int numberToBeSearched = 73;
+    LinearSearch  linearSearch = new LinearSearch();
+    BinarySearch binarySearch = new BinarySearch(dataSet, numberToBeSearched);
 
     public static void main(String[] args) {
     }
